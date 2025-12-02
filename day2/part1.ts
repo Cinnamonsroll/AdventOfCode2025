2e9d74f4494487de3de0a2d60f4151639869e35e
cinnamonsroll <cinnamonsrowolls@gmail.com>
2025-12-02 13:16:31 -0500

    Day 2: Add type for the line and use a for loop for the range instead of creating a one use array.

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,28 +1,27 @@
 import * as fs from "fs";
 
+type Line = `${number}-${number}`;
+
 const inputFile = "input.txt";
 
 const fileContent = fs.readFileSync(inputFile, "utf-8");
-const lines = fileContent.trim().split(",");
+const lines = fileContent.trim().split(",") as Line[];
 
 let totalWrongIds = 0;
 for (const line of lines) {
-  const [lowerRange, upperRange] = line.split("-").map(Number);
-  if (lowerRange && upperRange) {
-    const range = Array.from(
-      { length: upperRange - lowerRange + 1 },
-      (_, i) => i + lowerRange
-    ).map(String);
-
-    for (const id of range) {
-      if (id.length % 2 === 0) {
-        const mid = id.length / 2;
-        const left = id.slice(0, mid);
-        const right = id.slice(mid);
-        if (left === right) {
-          totalWrongIds += parseInt(id);
-          continue;
-        }
+  const [lowerRange, upperRange] = line.split("-").map(Number) as [
+    number,
+    number
+  ];
+  for (let i = 0; i <= upperRange - lowerRange; i++) {
+    const id = String(lowerRange + i);
+    if (id.length % 2 === 0) {
+      const mid = id.length / 2;
+      const left = id.slice(0, mid);
+      const right = id.slice(mid);
+      if (left === right) {
+        totalWrongIds += parseInt(id);
+        continue;
       }
     }
   }
--- day2/part2.ts
+++ day2/part2.ts
@@ -1,26 +1,25 @@
 import * as fs from "fs";
 
+type Line = `${number}-${number}`;
+
 const inputFile = "input.txt";
 
 const fileContent = fs.readFileSync(inputFile, "utf-8");
-const lines = fileContent.trim().split(",");
+const lines = fileContent.trim().split(",") as Line[];
 
 let totalWrongIds = 0;
 for (const line of lines) {
-  const [lowerRange, upperRange] = line.split("-").map(Number);
-  if (lowerRange && upperRange) {
-    const range = Array.from(
-      { length: upperRange - lowerRange + 1 },
-      (_, i) => i + lowerRange
-    ).map(String);
-
-    for (const id of range) {
-      const doubled = id + id;
-      const substring = doubled.slice(1, -1);
-      if (substring.includes(id)) {
-        totalWrongIds += parseInt(id);
-        continue;
-      }
+  const [lowerRange, upperRange] = line.split("-").map(Number) as [
+    number,
+    number
+  ];
+  for (let i = 0; i <= upperRange - lowerRange; i++) {
+    const id = String(lowerRange + i);
+    const doubled = id + id;
+    const substring = doubled.slice(1, -1);
+    if (substring.includes(id)) {
+      totalWrongIds += parseInt(id);
+      continue;
     }
   }
 }
