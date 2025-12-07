7388dfa5bd932bdc2ae95ebab04f15d01c7d0761
cinnamonsroll <cinnamonsrowolls@gmail.com>
2025-12-07 13:35:45 -0500

    Day 7 Solutions!

---
+++ day7/part1.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+
+const inputFile = "input.txt";
+
+const fileContent = fs.readFileSync(inputFile, "utf-8");
+const grid: string[] = fileContent.split(/\r?\n/);
+
+let beams = new Map([[grid[0].indexOf("S"), 1]]);
+let splitCount = 0;
+for(let i = 0; i < grid.length; i++){
+    const nextBeams: Map<number, number> = new Map();
+    for(const [beam, count] of beams.entries()){
+        if(grid[i]?.charAt(beam) === "."){
+            nextBeams.set(beam, (nextBeams.get(beam) ?? 0) + count)
+        }else{
+            nextBeams.set(beam - 1, (nextBeams.get(beam - 1) ?? 0) + count);
+            nextBeams.set(beam + 1, (nextBeams.get(beam + 1) ?? 0) + count);
+            splitCount++
+        }
+    }
+    beams = nextBeams
+}
+
+console.log(splitCount)
